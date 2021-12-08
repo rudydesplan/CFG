aacbf88faa4099819db902d0ceace28733dc18d6
rudy desplan <desrudy@hotmail.com>
2021-12-08 13:47:51 +0100

    Update Homework S4.py

--- Python Programming/Homework S4.py
+++ Python Programming/Homework S4.py
@@ -35,3 +35,39 @@ print(f"The price of is {type} chocolate is : {price}")
 # ● £100 for five matching numbers
 # ● £10000 for six matching numbers
 # ● £1000000 for seven matching numbers
+
+import random
+from random import randint
+
+my_lottery_ticket = []
+for number in range(0,7):
+    x = input("Please Enter your lottery number")
+    my_lottery_ticket += [x]
+print(my_lottery_ticket)
+
+winning_ticket = []
+for number in range(0,7):
+    y = randint(0, 100)
+    winning_ticket += [y]
+print(winning_ticket)
+
+matching_numbers = []
+for number in my_lottery_ticket:
+    if number in my_lottery_ticket:
+        matching_numbers += [number]
+print(matching_numbers)
+
+matching_numbers_count = len(matching_numbers)
+
+Prize = {
+    0 : "Sorry, no matching numbers.",
+    1 : "1 matching number. No win.",
+    2 : "2 matching numbers. No win.",
+    3 : "3 matching numbers. You won £20.",
+    4 : "4 matching numbers. You won £40.",
+    5 : "5 matching numbers. You won £100.",
+    6 : "6 matching numbers. You won £10'000!",
+    7 : "7 matching numbers. You won £1'000'000!!!"
+}
+
+print(Prize[matching_number_count])
